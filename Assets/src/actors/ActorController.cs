ec3d380cf6d98567b8a3f7f3b908a4fa2d4011a9
Samuel <samuel@morgan.ooo>
2021-03-07 21:34:26 +0000

    raycast working

---
+++ Assets/src/actors/ActorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ActorController : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
