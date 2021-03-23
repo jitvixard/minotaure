7e0c913db35bed5d40094d0bafa9146200c01f91
Samuel <samuel@morgan.ooo>
2021-03-23 23:58:37 +0000

    added loot delegates

--- Assets/src/services/impl/LootService.cs
+++ Assets/src/services/impl/LootService.cs
@@ -1,4 +1,3 @@
-using src.handlers;
 using src.scripting.level;
 using src.util;
 
@@ -6,6 +5,12 @@ namespace src.services.impl
 {
     public class LootService : IService
     {
+        public delegate void SetCard(Wave wave);
+        public event SetCard Card = delegate {  };
+        public delegate void SetScrap(int scrap);
+        public event SetScrap Scrap = delegate {  };
+        
+        
         Wave currentWave;
 
         public void Init()
